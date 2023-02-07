7ebc07e11306aba1f65c1c347f79fd78c784455e
Ashiq <ashiqnwu@gmail.com>
2023-02-07 12:13:17 +0600

    com

---
+++ assignment.js
@@ -0,0 +1,38 @@
+/*Assignment-1 start*/
+
+function mindGame(number){
+    var result = (((3 * number) + 10) / 2) - 5;
+    return result;
+}
+
+/*Assignment-1 end*/
+
+/*Assignment-2 start*/
+
+function evenOdd(string){
+    var stringLength = string.length;
+    var output = '';
+    if(stringLength%2 === 1){
+        output = 'odd';
+    }
+    else if(stringLength%2 === 0){
+        output = 'even';
+    }
+    return output;
+}
+/*Assignment-2 end*/
+
+/*Assignment-3 start*/
+
+
+/*Assignment-3 end*/
+
+/*Assignment-4 start*/
+
+
+/*Assignment-4 end*/
+
+/*Assignment-5 start*/
+
+
+/*Assignment-5 end*/
